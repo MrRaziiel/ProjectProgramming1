7154f8c2289e5d9668a609972a2f96354a485006
Ruben Amaro <ruben.amaro@wtvision.com>
2023-12-20 22:02:52 +0000

    added Games.html

---
+++ header.js
@@ -0,0 +1,9 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+    <header>
+    <h1>Sua Loja de Jogos</h1>
+    </header>
+`;
+
+document.body.appendChild(template.content);
